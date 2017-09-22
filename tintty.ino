2e0579209c4a8003dd22fecbab6c65ffb29a31a7
Nick Matantsev <nick.matantsev@gmail.com>
2017-09-22 03:07:41 -0400

    Change up test sequences

--- tintty.ino
+++ tintty.ino
@@ -72,14 +72,21 @@ void setup() {
   delay(200);
   Keyboard.SetReportParser(0, (HIDReportParser*)&tinTTYKeyboardParser); // @todo needs delay?
 
-//  test_buffer_puts("Hi!\nSecond\nThird\rStart\r\nt\te\ts\tt\bT\eM\eM1\b\eD2\eET\b\b\e[200B");
-//  test_buffer_puts("\e[3;2f[3;2]");
-//  test_buffer_puts("\e[14;9H[14;9]");
-//  test_buffer_puts("\e[6;1H\e[32;40mSome \e[1mgreen\e[0;32m text\e[m (and back to \e[47;30mnormal\e[m)");
-//  test_buffer_puts("\e7"); // save
-//  test_buffer_puts("\e[14;20H\e[?7l0123456789\e[?7hnext line");
-//  test_buffer_puts("\e[34l\e[?25h"); // cursor off and on
-//  test_buffer_puts("\e8"); // restore
+  // test_buffer_puts("----\n");
+  // test_buffer_puts("Hi!!\b\b");
+  // test_buffer_puts("\e7"); // save
+  // test_buffer_puts("\e[16;9H[16;9]");
+  // test_buffer_puts("\ntest?");
+  // test_buffer_puts("\e8"); // restore
+  // test_buffer_puts("\e[2K"); // clear
+  // test_buffer_puts("Hi!\nSecond\nThird\rStart\r\nt\te\ts\tt\bT\eM\eM1\b\eD2\eET\b\b\e[200B");
+  // test_buffer_puts("\e[3;2f[3;2]");
+  // test_buffer_puts("\e[14;9H[14;9]");
+  // test_buffer_puts("\e[6;1H\e[32;40mSome \e[1mgreen\e[0;32m text\e[m (and back to \e[47;30mnormal\e[m)");
+  // test_buffer_puts("\e7"); // save
+  // test_buffer_puts("\e[14;20H\e[?7l0123456789\e[?7hnext line");
+  // test_buffer_puts("\e[34l\e[?25h"); // cursor off and on
+  // test_buffer_puts("\e8"); // restore
 
   tintty_run(
     [=](){
