8530e4aa0a2905e7130ea0d3818f5e53d7b2a616
Nick Matantsev <nick.matantsev@gmail.com>
2017-10-20 18:25:56 -0400

    Split USB keyboard code into separate module

--- tintty.ino
+++ tintty.ino
@@ -11,74 +11,13 @@
 #include <Adafruit_GFX.h>
 #include <TFT_ILI9163C.h>
 
-#include <Usb.h>
-#include <hidboot.h>
-
-// extras from https://github.com/arduino/ArduinoCore-samd/commit/0be91985fd090855e3157a8729d88cc608ed90f2
-#define UHS_HID_BOOT_KEY_ESCAPE 0x29
-#define UHS_HID_BOOT_KEY_DELETE 0x2a
-#define UHS_HID_BOOT_KEY_TAB 0x2b
-
 #include "tintty.h"
+#include "tintty_usbkeyboard.h"
 
 // this is following a tutorial-recommended pinout, but changing
 // the SPI CS and RS pins for the TFT to be 5 and 4 to avoid
 // device conflict with USB Host Shield
 TFT_ILI9163C tft = TFT_ILI9163C(5, 8, 4);
-
-// USB Host Shield connection
-USB Usb;
-HIDBoot<USB_HID_PROTOCOL_KEYBOARD> Keyboard(&Usb);
-
-class TinTTYKeyboardParser : public KeyboardReportParser {
-protected:
-    virtual void OnKeyDown(uint8_t mod, uint8_t key);
-    virtual void OnKeyPressed(uint8_t key);
-};
-
-void TinTTYKeyboardParser::OnKeyDown(uint8_t mod, uint8_t key) {
-    // process control keys because they are not supported by AVR UsbHost?
-    if (mod & 0x11) {
-        // Ctrl + [A-Z]
-        // per https://github.com/arduino/ArduinoCore-samd/commit/0be91985fd090855e3157a8729d88cc608ed90f2
-        if (key >= 0x04 && key <= 0x1d)  {
-            OnKeyPressed(key - 3);
-        }
-
-        return;
-    }
-
-    uint8_t asciiChar = OemToAscii(mod, key);
-
-    if (asciiChar) {
-        OnKeyPressed(asciiChar);
-    } else {
-        // extra unsupported characters
-        switch (key) {
-            case UHS_HID_BOOT_KEY_ESCAPE:
-                OnKeyPressed('\e');
-                break;
-
-            case UHS_HID_BOOT_KEY_DELETE:
-                OnKeyPressed('\b');
-                break;
-
-            case UHS_HID_BOOT_KEY_TAB:
-                OnKeyPressed('\t');
-                break;
-        }
-    }
-}
-
-void TinTTYKeyboardParser::OnKeyPressed(uint8_t asciiChar) {
-    // send character as is, correcting 0x13 (DC3) into 0x0D (CR)
-    Serial.print((char)(asciiChar == 0x13
-        ? 0x0D
-        : asciiChar
-    ));
-};
-
-TinTTYKeyboardParser tinTTYKeyboardParser;
 
 // buffer to test various input sequences
 char *test_buffer = "-- \e[1mTinTTY\e[m --\r\n";
@@ -87,11 +26,9 @@ uint8_t test_buffer_cursor = 0;
 void setup() {
   Serial.begin(9600); // normal baud-rate
 
-  tft.begin();
+  tintty_usbkeyboard_init();
 
-  Usb.Init();
-  delay(200);
-  Keyboard.SetReportParser(0, (HIDReportParser*)&tinTTYKeyboardParser); // @todo needs delay?
+  tft.begin();
 
   tintty_run(
     [=](){
@@ -113,15 +50,14 @@ void setup() {
     [=](){
       // process at least one idle loop to allow input to happen
       tintty_idle(&tft);
-      Usb.Task(); // @todo move?
+      tintty_usbkeyboard_update(); // @todo move?
 
       // first read from the test buffer
       char test_char = test_buffer[test_buffer_cursor];
 
       if (test_char) {
         tintty_idle(&tft);
-
-        Usb.Task(); // @todo move?
+        tintty_usbkeyboard_update(); // @todo move?
 
         test_buffer_cursor += 1;
         return test_char;
@@ -130,7 +66,7 @@ void setup() {
       // fall back to normal blocking serial input
       while (Serial.available() < 1) {
         tintty_idle(&tft);
-        Usb.Task(); // @todo move?
+        tintty_usbkeyboard_update(); // @todo move?
       }
 
       return (char)Serial.read();
---
+++ tintty_usbkeyboard.h
@@ -0,0 +1,13 @@
+/**
+ * USB keyboard to serial.
+ */
+
+/**
+ * Initialization
+ */
+void tintty_usbkeyboard_init();
+
+/**
+ * Hook to call while e.g. sketch is waiting for input
+ */
+void tintty_usbkeyboard_update();
