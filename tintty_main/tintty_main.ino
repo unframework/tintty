e0ac2078b8b4a6f51c37da775617acab97d43713
Nick Matantsev <nick.matantsev@gmail.com>
2018-12-30 18:08:19 -0500

    Add simple input forwarding via serial RX

--- tintty_main/tintty_main.ino
+++ tintty_main/tintty_main.ino
@@ -7,6 +7,7 @@
  */
 
 #include <SPI.h>
+#include <SoftwareSerial.h>
 
 #include <Adafruit_GFX.h>
 #include <TFT_ILI9163C.h>
@@ -16,7 +17,11 @@
 // this is following a tutorial-recommended pinout, but changing
 // the SPI CS and RS pins for the TFT to be 5 and 4 to avoid
 // device conflict with USB Host Shield
+// @todo rethink, now that USB Host Shield is not used directly
 TFT_ILI9163C tft = TFT_ILI9163C(5, 8, 4);
+
+// input serial forwarder RX, TX (TX should not be used anyway)
+SoftwareSerial inputSerial(9, 10);
 
 // buffer to test various input sequences
 char *test_buffer = "-- \e[1mTinTTY\e[m --\r\n";
@@ -24,6 +29,7 @@ uint8_t test_buffer_cursor = 0;
 
 void setup() {
   Serial.begin(9600); // normal baud-rate
+  inputSerial.begin(9600); // normal baud-rate
 
   tft.begin();
 
@@ -47,12 +53,14 @@ void setup() {
     [=](){
       // process at least one idle loop to allow input to happen
       tintty_idle(&tft);
+      input_idle();
 
       // first read from the test buffer
       char test_char = test_buffer[test_buffer_cursor];
 
       if (test_char) {
         tintty_idle(&tft);
+        input_idle();
 
         test_buffer_cursor += 1;
         return test_char;
@@ -61,6 +69,7 @@ void setup() {
       // fall back to normal blocking serial input
       while (Serial.available() < 1) {
         tintty_idle(&tft);
+        input_idle();
       }
 
       return (char)Serial.read();
@@ -71,4 +80,10 @@ void setup() {
 }
 
 void loop() {
+}
+
+void input_idle() {
+  if (inputSerial.available()) {
+    Serial.write(inputSerial.read());
+  }
 }
